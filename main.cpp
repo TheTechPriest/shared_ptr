1b273a1e589b9716f238430a23034f80174560ba
TheTechPriest <corentin.veran@xour.fr>
2026-03-26 15:59:47 +0100

    Adding control block implementation

--- main.cpp
+++ main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 
 template <typename T> class ControlBlock {
+public:
   T *object;
-  int shared_ptrs{};
-  int weak_ptrs{};
+  int shared_ptrs{1};
+  int weak_ptrs{0};
+
+  ControlBlock(T *newObject) : object{newObject} {};
+
+  void print_value() {
+    std::cout << "Control block value: " << *object << std::endl;
+  }
 };
 
 template <typename T> class sharedPtr {
 private:
   T *m_ptr;
+  ControlBlock<T> control;
 
 public:
-  sharedPtr(T *ptr = nullptr) {
-    m_ptr = ptr;
+  sharedPtr(T *ptr = nullptr)
+      : m_ptr{ptr}, control{ptr} { // Assigning default as nullptr if no element
+                                   // is provided
     std::cout << "Initialized new shared pointer with value: " << *m_ptr
               << std::endl;
+
+    control.print_value();
   }
 
-  int use_count() { return }
+  T *get() { return m_ptr; }
+
+  int use_count() { return control.shared_ptrs; }
 };
 
 int main() {
