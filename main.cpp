978384e520636e28a00b03b0ce8322aeeff107d4
TheTechPriest <corentin.veran@xour.fr>
2026-03-27 16:27:48 +0100

    control block properly heap-allocated

--- main.cpp
+++ main.cpp
@@ -1,34 +1,56 @@
+#include <atomic>
 #include <iostream>
+#include <memory>
+
+// Custom Shared pointer implementation for learning purposes
 
 template <typename T> class ControlBlock {
 public:
   T *object;
-  int shared_ptrs{1};
-  int weak_ptrs{0};
+  // Using atomic for safety during multithreading operations
+  std::atomic<int> shared_ptrs{1};
+  std::atomic<int> weak_ptrs{0};
 
-  ControlBlock(T *newObject) : object{newObject} {};
+  ControlBlock(T *newObject = nullptr) : object{newObject} {};
 
   void print_value() {
     std::cout << "Control block value: " << *object << std::endl;
+  }
+
+  void deleter() {
+    // Using -- since it's properly overloaded in the= atomic class
+    shared_ptrs--;
+    if (shared_ptrs == 0) {
+      delete object;
+      if (weak_ptrs == 0) {
+        delete this;
+        return;
+      }
+    }
   }
 };
 
 template <typename T> class sharedPtr {
 private:
-  T *m_ptr;
-  ControlBlock<T> control;
+  T *storedPtr;
+  ControlBlock<T> *control;
 
 public:
+  // Constructor
+  // Assigning default as nullptr if
+  // no element is provided
   sharedPtr(T *ptr = nullptr)
-      : m_ptr{ptr}, control{ptr} { // Assigning default as nullptr if no element
-                                   // is provided
-    std::cout << "Initialized new shared pointer with value: " << *m_ptr
+      : storedPtr{ptr}, control{new ControlBlock<T>(ptr)} {
+    std::cout << "Initialized new shared pointer with value: " << *storedPtr
               << std::endl;
 
-    control.print_value();
+    control->print_value();
   }
 
-  T *get() { return m_ptr; }
+  // Destructor
+  ~sharedPtr() { control->deleter(); }
+
+  T *get() { return storedPtr; }
 
   int use_count() { return control.shared_ptrs; }
 };
