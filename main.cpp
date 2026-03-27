99854a862f3d7bcc891befeec04a4d3a41246886
TheTechPriest <corentin.veran@xour.fr>
2026-03-27 19:14:37 +0100

    added test for concurrency and reset() function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <atomic>
 #include <iostream>
 #include <memory>
+#include <mutex>
+#include <thread>
+
+using namespace std::chrono_literals;
 
 // Custom Shared pointer implementation for learning purposes
 
@@ -23,6 +27,8 @@ public:
     if (shared_ptrs == 0) {
       delete object;
       if (weak_ptrs == 0) {
+
+        std::cout << "Deleting control block\n";
         delete this;
         return;
       }
@@ -60,6 +66,16 @@ public:
 
   // Operator overloading
 
+  void reset(T *ptr = nullptr) {
+    control->deleter();
+
+    storedPtr = ptr;
+
+    if (storedPtr) {
+      control = new ControlBlock<T>(storedPtr);
+    }
+  }
+
   T &operator*() { return *storedPtr; }
   T *operator->() { return storedPtr; }
 
@@ -85,12 +101,15 @@ public:
 
 void printSharedPointerTest() {
 
+  std::cout << "===REFERENCE COUNT TEST===\n";
+
   sharedPtr<int> ptr1(new int(151));
 
   std::cout << "Use count for ptr1: " << ptr1.use_count() << std::endl;
 
   sharedPtr<int> ptr2 = ptr1;
 
+  // Use count increments
   std::cout << "Use count for ptr1: " << ptr1.use_count() << std::endl;
   std::cout << "Use count for ptr2: " << ptr2.use_count() << std::endl;
 
@@ -108,11 +127,43 @@ void printSharedPointerTest() {
   std::cout << "Use count for ptr2: " << ptr2.use_count() << std::endl;
 }
 
+// Mostly copied from the cppref shared_pointer article
+void thr(sharedPtr<int> p) {
+  std::this_thread::sleep_for(987ms);
+  sharedPtr<int> lp = p;
+  {
+    static std::mutex io_mutex;
+    std::lock_guard<std::mutex> lk(io_mutex);
+    std::cout << "Local pointer in a thread: " << lp.get()
+              << ", Use count: " << lp.use_count() << std::endl;
+  }
+}
+
+void printConcurrencyTest() {
+
+  std::cout << "===CONCURRENCY TEST===\n";
+
+  sharedPtr<int> ptr(new int(100));
+
+  std::cout << "Use count in main thread: " << ptr.use_count() << std::endl;
+  std::thread t1{thr, std::ref(ptr)}, t2{thr, std::ref(ptr)},
+      t3{thr, std::ref(ptr)};
+
+  // Can use reset() to release ownership from main
+  // ptr.reset();
+
+  t1.join();
+  t2.join();
+  t3.join();
+
+  std::cout << "Use count in main thread: " << ptr.use_count() << std::endl;
+}
+
 int main() {
 
-  // static_assert(true, "test123");
-
   printSharedPointerTest();
+
+  printConcurrencyTest();
 
   return 0;
 }
